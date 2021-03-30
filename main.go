ab1d4bc9f33904ae5fa641d9a1228706c0c54e91
matansharon <49459070+matansharon@users.noreply.github.com>
2021-03-30 10:57:07 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+	list_of_cards := new_deck()
+	// hand, rest_of_deck := deal(list_of_cards, 5)
+	// hand.print()
+	// rest_of_deck.print()
+	// fmt.Println(list_of_cards.to_byte_slice(list_of_cards))
+
+}
