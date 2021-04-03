868ca39bc19a3160d84b66ebb5dc054b84c3c542
matansharon <49459070+matansharon@users.noreply.github.com>
2021-04-03 22:07:24 +0300

    Add files via upload

--- deck.go
+++ deck.go
@@ -1,36 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
-type deck [] string
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
 
-func (d deck) print(){
-	for i,card := range d{
-		
-		fmt.Println(i,card)
-	}
-}
+func newDeck() deck {
+	cards := deck{}
 
-func new_deck() deck{
-	cards:= deck{}
-	suiets := []string{"Spades","Diamonds","Hearts","Clubs"}
-	values:= []string{"Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"}
-	for _,suit := range suiets{
-		for _,value := range values{
-			cards=append(cards,suit+" of "+value)
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
 		}
 	}
+
 	return cards
 }
 
-func deal (d deck, hand int)(deck,deck){
-	return d[:hand],d[hand:]
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
 }
 
-func (d deck)to_byte_slice([]byte){
-	st:=""
-	for _,card := range d{
-		st=append(st,card+" ")
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - log the error and return a call to newDeck()
+		// Option #2 - Log the error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
 	}
-	return []byte(st)
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
